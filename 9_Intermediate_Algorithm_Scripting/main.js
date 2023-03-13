b3c363a7a30f8e1fccca6cc08bd8841179899b1d
devjean <jeancarloleall@gmail.com>
2023-03-13 19:35:18 -0300

    Intermediate algorithm scripting

---
+++ 9_Intermediate_Algorithm_Scripting/main.js
@@ -0,0 +1,7 @@
+// Passaremos a você um array de dois números. Retorne a soma desses dois números mais a soma de todos os números entre eles. O número mais baixo nem sempre virá primeiro.
+function sumAll(arr) {
+  return 1;
+}
+
+sumAll([1, 4]);
+// ================================================================
